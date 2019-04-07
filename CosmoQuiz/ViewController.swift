7caba95828aa6b6f42287c3613df1852734f05f5
pabloegpf1 <pabloescrivagallardo@gmail.com>
2019-04-07 02:45:55 -0700

    Displays images from Nasa's API

--- CosmoQuiz/ViewController.swift
+++ CosmoQuiz/ViewController.swift
@@ -8,28 +8,38 @@
 
 import UIKit
 import Alamofire
+import AlamofireImage
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var ImageOutlet:[UIImageView]!
     
     let API_KEY = "uBHdRJkKAl917xAmxxaAqtRig5PtTzotpfw5v9v4"
     let photoCount = 4
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let url = "https://api.nasa.gov/planetary/apod?api_key=\(API_KEY)&count=\(photoCount)"
-        
-        Alamofire.request(url).responseJSON(completionHandler:{ response in
-            guard
-                let json = response.result.value as? [String:Any]
-            else{
-                print("Error retrieving JSON")
-                return
-            }
-            print("JSON: \(json)")
-        })
-        
     }
+    
+    @IBAction func refreshButtonPressed(_ sender: Any) {
+        let url = "https://api.nasa.gov/planetary/apod?api_key=\(API_KEY)&count=\(photoCount)"
+        var responseJson:Array<[String:AnyObject]>  = []
+        Alamofire.request(url).responseJSON(completionHandler:{ response in
+            responseJson = response.result.value as! Array<[String:AnyObject]>
+            for i in 0 ... self.photoCount-1{
+                let ImageURL = URL(string: responseJson[i]["url"] as! String)!
+                Alamofire.request(ImageURL).responseData { (response) in
+                    if response.error == nil {
+                        print(response.result)
+                        if let data = response.data {
+                            self.ImageOutlet[i].image = UIImage(data: data)
+                        }
+                    }
+                }
+            }
+        })
+    }
+    
 
 
 }
